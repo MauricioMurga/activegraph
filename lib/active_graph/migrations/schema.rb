061e47b523229e89d19985599aabe6e0ec2ac886
Mauricio Murga <murga.mauricio@gmail.com>
2023-01-11 12:40:16 -0600

    fix weird bug

--- lib/active_graph/migrations/schema.rb
+++ lib/active_graph/migrations/schema.rb
@@ -24,28 +24,49 @@ module ActiveGraph
         end
 
         def fetch_index_descriptions
-          ActiveGraph::Base.raw_indexes do |keys, result|
-            if keys.include?(:description)
-              v3_indexes(result)
+          result = ActiveGraph::Base.raw_indexes
+          result.reject do |row|
+            byebug
+            if row.keys.include?(:description)
+              # v3 indexes
+              row[:type] == 'node_unique_property'
             else
-              v4_indexes(result)
+              # v4 indexes
+              row[:uniqueness] == 'UNIQUE'
+            end
+          end.map do |row|
+            if row.keys.include?(:description)
+              row[:description]
+            else
+              row.try(:description)
             end
           end
         end
 
-        def v3_indexes(result)
-          result.reject do |row|
-            # These indexes are created automagically when the corresponding constraints are created
-            row[:type] == 'node_unique_property'
-          end.map { |row| row[:description] }
-        end
+        # The code below does not seem to work as intended
+        # def fetch_index_descriptions
+        #   ActiveGraph::Base.raw_indexes do |keys, result|
+        #     if keys.include?(:description)
+        #       v3_indexes(result)
+        #     else
+        #       v4_indexes(result)
+        #     end
+        #   end
+        # end
 
-        def v4_indexes(result)
-          result.reject do |row|
-            # These indexes are created automagically when the corresponding constraints are created
-            row[:uniqueness] == 'UNIQUE'
-          end.map(&method(:description))
-        end
+        # def v3_indexes(result)
+        #   result.reject do |row|
+        #     # These indexes are created automagically when the corresponding constraints are created
+        #     row[:type] == 'node_unique_property'
+        #   end.map { |row| row[:description] }
+        # end
+
+        # def v4_indexes(result)
+        #   result.reject do |row|
+        #     # These indexes are created automagically when the corresponding constraints are created
+        #     row[:uniqueness] == 'UNIQUE'
+        #   end.map(&method(:description))
+        # end
 
         def description(row)
           "INDEX FOR (n:#{row[:labelsOrTypes].first}) ON (#{row[:properties].map { |prop| "n.#{prop}" }.join(', ')})"
